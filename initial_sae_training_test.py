b6797a4a2773f5faed7380710c3105bfa59a22c7
HugoFry <hugo.fry@fryfamily.co.uk>
2024-02-08 15:20:16 -0800

    Full training run test with all html outputs
    Former-commit-id: 77721865cfdc6a4453f3a00bf4413622b1e97e46 [formerly 26a66a7cf8bc94f190f7c3ba411f7e188469887b]
    Former-commit-id: fdce0c057400dede0955c9c7d7b864852680ffef

--- initial_sae_training_test.py
+++ initial_sae_training_test.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import torch
+import tqdm
 import wandb
 import json
 import plotly.express as px
@@ -36,8 +37,8 @@ cfg = LanguageModelSAERunnerConfig(
 
     # Data Generating Function (Model + Training Distibuion)
     model_name = "gpt2-small",
-    hook_point = "blocks.2.hook_resid_pre",
-    hook_point_layer = 2,
+    hook_point = "blocks.10.hook_resid_pre",
+    hook_point_layer = 10,
     d_in = 768,
     dataset_path = "Skylion007/openwebtext",
     is_dataset_tokenized=False,
@@ -49,7 +50,7 @@ cfg = LanguageModelSAERunnerConfig(
     # Training Parameters
     lr = 0.0004,
     l1_coefficient = 0.00008,
-    lr_scheduler_name="constantwithwarmup",
+    lr_scheduler_name="cosineannealingwarmup",
     train_batch_size = 4096,
     context_size = 128,
     lr_warm_up_steps=5000,
@@ -96,6 +97,100 @@ sparse_autoencoder = train_sae_on_language_model(
     feature_sampling_method = None,
     use_wandb = True,
 )
+
+#Evaluate the SAE in terms of the models loss and compare to zero/mean ablation.
+wandb.init(project='mats-hugo', entity='hugo-fry', job_type="inference")
+
+def reconstr_hook(mlp_out, hook, new_mlp_out):
+    return new_mlp_out
+
+def zero_abl_hook(mlp_out, hook):
+    return torch.zeros_like(mlp_out)
+
+batch_tokens = activations_store.get_batch_tokens()
+_, cache = model.run_with_cache(batch_tokens, prepend_bos=True)
+_, cache = model.run_with_cache(batch_tokens, prepend_bos=True)
+sae_out, feature_acts, loss, mse_loss, l1_loss = sparse_autoencoder(
+    cache[sparse_autoencoder.cfg.hook_point]
+    )
+del cache
+
+original_loss = model(batch_tokens, return_type="loss").item()
+sae_reconstruction_loss = model.run_with_hooks(
+        batch_tokens,
+        fwd_hooks=[
+            (
+                utils.get_act_name("resid_pre", 10),
+                partial(reconstr_hook, new_mlp_out=sae_out),
+            )
+        ],
+        return_type="loss",
+    ).item(),
+
+zero_ablation_loss = model.run_with_hooks(
+        batch_tokens,
+        return_type="loss",
+        fwd_hooks=[(utils.get_act_name("resid_pre", 10), zero_abl_hook)],
+    ).item()
+
+wandb.log({'original loss':original_loss, 'sae loss':sae_reconstruction_loss, 'zero ablation loss':zero_ablation_loss})
+wandb.finish()
+
+
+vocab_dict = model.tokenizer.vocab
+vocab_dict = {v: k.replace("Ġ", " ").replace("\n", "\\n") for k, v in vocab_dict.items()}
+
+vocab_dict_filepath = Path(os.getcwd()) / "vocab_dict.json"
+if not vocab_dict_filepath.exists():
+    with open(vocab_dict_filepath, "w") as f:
+        json.dump(vocab_dict, f)
+        
+
+os.environ["TOKENIZERS_PARALLELISM"] = "false"
+data = activations_store.dataset
+tokenized_data = utils.tokenize_and_concatenate(data, model.tokenizer, max_length=128)
+tokenized_data = tokenized_data.shuffle(42)
+all_tokens = tokenized_data["tokens"]
+
+
+# Currently, don't think much more time can be squeezed out of it. Maybe the best saving would be to
+# make the entire sequence indexing parallelized, but that's possibly not worth it right now.
+
+max_batch_size = 512
+total_batch_size = 4096*5
+feature_idx = list(range(sparse_autoencoder.d_sae))
+# max_batch_size = 512
+# total_batch_size = 16384
+# feature_idx = list(range(1000))
+
+tokens = all_tokens[:total_batch_size]
+
+feature_data: Dict[int, FeatureData] = get_feature_data(
+    encoder=sparse_autoencoder,
+    # encoder_B=sparse_autoencoder,
+    model=model,
+    hook_point=sparse_autoencoder.cfg.hook_point,
+    hook_point_layer=sparse_autoencoder.cfg.hook_point_layer,
+    tokens=tokens,
+    feature_idx=feature_idx,
+    max_batch_size=max_batch_size,
+    left_hand_k = 3,
+    buffer = (5, 5),
+    n_groups = 10,
+    first_group_size = 20,
+    other_groups_size = 5,
+    verbose = True,
+)
+
+pbar=tqdm(total = len(feature_idx))
+
+for test_idx in feature_idx:
+    html_str = feature_data[test_idx].get_all_html()
+    with open(f"data_{test_idx:04}.html", "w") as f:
+        f.write(html_str)
+    pbar.update(1)
+    
+pbar.close()
 
 for i in range(4):
     print()
