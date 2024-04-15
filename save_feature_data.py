92afbe5179b41eb47af6cdad90db6d57a4844536
hugofry <hugo.fry@fryfamily.co.uk>
2024-04-15 23:52:54 +0000

    sparsity histogram
    Former-commit-id: edc51cbada3be796fbc8974bcbc3d83fb5e86668 [formerly 62752f6eff105f70d1e4078dd161c9d60a044e5c]
    Former-commit-id: e58db1e33599305562958ace949dda05034c729b

--- save_feature_data.py
+++ save_feature_data.py
@@ -10,18 +10,16 @@ from typing import  Dict
 from pathlib import Path
 from tqdm import tqdm
 from functools import partial
-from vit_sae_analysis.dashboard_fns import get_feature_data, FeatureData
+from vit_sae_analysis.dashboard_fns import get_feature_data
 
 
 sys.path.append("..")
 
 from sae_training.utils import LMSparseAutoencoderSessionloader
 from sae_analysis.visualizer import data_fns, html_fns
-from sae_analysis.visualizer.data_fns import get_feature_data, FeatureData
 from sae_training.config import ViTSAERunnerConfig
 from sae_training.vit_runner import vision_transformer_sae_runner
 from sae_training.train_sae_on_vision_transformer import train_sae_on_vision_transformer
-from vit_sae_analysis.dashboard_fns import get_feature_data, FeatureData
 from sae_training.sparse_autoencoder import SparseAutoencoder
 from sae_training.utils import ViTSparseAutoencoderSessionloader
 
@@ -53,7 +51,6 @@ model.to(cfg.device)
 get_feature_data(
     sparse_autoencoder,
     model,
-    number_of_images = 65_536,
+    number_of_images = 131_072,
+    load_pretrained=True,
 )
-
-
