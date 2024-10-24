974fdf7f160d955b60b4b0df3760069c286bb014
Samuel Stevens <samuel.robert.stevens@gmail.com>
2024-10-24 17:51:35 -0400

    Optionally disable SSL if needed

--- main.py
+++ main.py
@@ -1,15 +1,38 @@
+import logging
 import typing
 
 import tyro
 
 import saev
 
+log_format = "[%(asctime)s] [%(levelname)s] [%(name)s] %(message)s"
+logging.basicConfig(level=logging.INFO, format=log_format)
+logger = logging.getLogger("main")
 
-def activations(cfg: typing.Annotated[saev.Config, tyro.conf.arg(name="")]):
-    """Save ViT activations for use later on."""
+
+def activations(
+    cfg: typing.Annotated[saev.Config, tyro.conf.arg(name="")],
+    disable_ssl: bool = False,
+):
+    """
+    Save ViT activations for use later on.
+
+    Args:
+        cfg: Configuration for experiment.
+        disable_ssl: Whether to ignore ssl when downloading models and such.
+    """
+    if disable_ssl:
+        logger.warning("Ignoring SSL certs. Try not to do this!")
+        # https://github.com/openai/whisper/discussions/734#discussioncomment-4491761
+        # Ideally we don't have to disable SSL but we are only downloading weights.
+        import ssl
+
+        ssl._create_default_https_context = ssl._create_unverified_context
+
     import saev.modeling
 
     vit = saev.modeling.RecordedVit(cfg)
+    logger.info("Loaded ViT '%s'.", cfg.model)
     saev.modeling.save_acts(cfg, vit)
 
 
@@ -38,3 +61,4 @@ if __name__ == "__main__":
         "activations": activations,
         "train": train,
     })
+    logger.info("Done.")
