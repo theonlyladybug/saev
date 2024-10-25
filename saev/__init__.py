3e8132b00c99b57d62e8b487d4bdace7bef9a19c
Samuel Stevens <samuel.robert.stevens@gmail.com>
2024-10-25 09:46:05 -0400

    Updating docs

--- saev/__init__.py
+++ saev/__init__.py
@@ -1,3 +1,9 @@
+"""
+saev is a Python package for training sparse autoencoders (SAEs) on vision transformers (ViTs) in PyTorch.
+
+The main entrypoint to the package is in [main.py](https://github.com/samuelstevens/saev/blob/main/main.py); use `python main.py --help` to see the options and documentation for the script.
+"""
+
 from .config import Config
 
 __all__ = ["Config"]
