d05db88cb36fbf9cb98b9f8540474b46b0692f60
Samuel Stevens <samuel.robert.stevens@gmail.com>
2025-03-03 12:59:35 -0500

    add RGB colors

--- saev/colors.py
+++ saev/colors.py
@@ -1,24 +1,69 @@
-BLACK = "001219"
-GREEN = "005f73"
-CYAN = "0a9396"
-BLUE = "94d2bd"
-CREAM = "e9d8a6"
-GOLD = "ee9b00"
-ORANGE = "ca6702"
-RUST = "bb3e03"
-SCARLET = "ae2012"
-RED = "9b2226"
+# https://coolors.co/palette/001219-005f73-0a9396-94d2bd-e9d8a6-ee9b00-ca6702-bb3e03-ae2012-9b2226
+
+BLACK_HEX = "001219"
+BLACK_RGB = (0, 18, 25)
+BLACK_RGB01 = tuple(c / 256 for c in BLACK_RGB)
 
 
-ALL = [
-    BLACK,
-    GREEN,
-    CYAN,
-    BLUE,
-    CREAM,
-    GOLD,
-    ORANGE,
-    RUST,
-    SCARLET,
-    RED,
+BLUE_HEX = "005f73"
+BLUE_RGB = (0, 95, 115)
+BLUE_RGB01 = tuple(c / 256 for c in BLUE_RGB)
+
+
+CYAN_HEX = "0a9396"
+CYAN_RGB = (10, 147, 150)
+CYAN_RGB01 = tuple(c / 256 for c in CYAN_RGB)
+
+SEA_HEX = "94d2bd"
+SEA_RGB = (148, 210, 189)
+SEA_RGB01 = tuple(c / 256 for c in SEA_RGB)
+
+CREAM_HEX = "e9d8a6"
+CREAM_RGB = (233, 216, 166)
+CREAM_RGB01 = tuple(c / 256 for c in CREAM_RGB)
+
+GOLD_HEX = "ee9b00"
+GOLD_RGB = (238, 155, 0)
+GOLD_RGB01 = tuple(c / 256 for c in GOLD_RGB)
+
+ORANGE_HEX = "ca6702"
+ORANGE_RGB = (202, 103, 2)
+ORANGE_RGB01 = tuple(c / 256 for c in ORANGE_RGB)
+
+RUST_HEX = "bb3e03"
+RUST_RGB = (187, 62, 3)
+RUST_RGB01 = tuple(c / 256 for c in RUST_RGB)
+
+SCARLET_HEX = "ae2012"
+SCARLET_RGB = (174, 32, 18)
+SCARLET_RGB01 = tuple(c / 256 for c in SCARLET_RGB)
+
+RED_HEX = "9b2226"
+RED_RGB = (155, 34, 38)
+RED_RGB01 = tuple(c / 256 for c in RED_RGB)
+
+
+ALL_HEX = [
+    BLACK_HEX,
+    BLUE_HEX,
+    CYAN_HEX,
+    SEA_HEX,
+    CREAM_HEX,
+    GOLD_HEX,
+    ORANGE_HEX,
+    RUST_HEX,
+    SCARLET_HEX,
+    RED_HEX,
+]
+ALL_RGB01 = [
+    BLACK_RGB01,
+    BLUE_RGB01,
+    CYAN_RGB01,
+    SEA_RGB01,
+    CREAM_RGB01,
+    GOLD_RGB01,
+    ORANGE_RGB01,
+    RUST_RGB01,
+    SCARLET_RGB01,
+    RED_RGB01,
 ]
